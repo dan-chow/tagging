ff4e5249d49c7a545902283a2b8fe12bd435ac30
yhx <scbzyhx@dislab.nju.edu.cn>
2016-01-03 16:51:32 +0800

    test OK

---
+++ sh.sh
@@ -0,0 +1,20 @@
+#! /bin/sh
+
+#the interface shoulde be specified when used in edge router(ISP router)
+IN_INTF="-i eth0"
+OUT_INTF="-o eth0"
+#ingress
+iptables -t mangle -A PREROUTING ${IN_INTF} -m dscp --dscp 46 -j MARK --set-mark 46
+iptables -t mangle -A PREROUTING ${IN_INTF} -m dscp --dscp 16 -j MARK --set-mark 16
+#iptables -t mangle -A PREROUTING -m mark ! --mark 0 -j ACCEPT
+    
+#iptables -t mangle -A PREROUTING -j MARK --set-mark 1 # we don't care about this
+iptables -t mangle -A PREROUTING ${IN_INTF} -j CONNMARK --save-mark
+    
+    
+#egress
+    
+iptables -t mangle -A POSTROUTING ${OUT_INTF} -j CONNMARK --restore-mark
+iptables -t mangle -A POSTROUTING ${OUT_INTF} -m mark --mark 46 -j DSCP --set-dscp 46
+iptables -t mangle -A POSTROUTING ${OUT_INTF} -m mark --mark 16 -j DSCP --set-dscp 16
+#that's all
